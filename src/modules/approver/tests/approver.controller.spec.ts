0dae9e204734a6d4974e406405ea3b43f9c71391
soehtetlin <soehtetlin@myanmardcr.com>
2026-06-24 08:51:49 +0630

    test: add unit tests for ApproverController functionality

--- src/modules/approver/tests/approver.controller.spec.ts
+++ src/modules/approver/tests/approver.controller.spec.ts
@@ -6,7 +6,7 @@ import { ApprovePaymentRequestDto } from '../dto/approve-payment-request.dto';
 import { RejectPaymentRequestDto } from '../dto/reject-payment-request.dto';
 import { JwtPayload } from '../../shared/types';
 
-describe('ApprController', () => {
+describe('ApproverController', () => {
   let controller: ApproverController;
   let service: jest.Mocked<ApproverService>;
 
